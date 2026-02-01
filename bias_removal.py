7f46be42dfd53e13f731933c4599d47358bf5aec
shirke3003 <anushkashirke30@gmail.com>
2026-02-02 03:27:23 +0530

    Delete bias_removal.py

--- bias_removal.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def remove_pii(text: str) -> str:
-    text = re.sub(r"\b[A-Z][a-z]+ [A-Z][a-z]+\b", "REDACTED_NAME", text)
-    text = re.sub(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b", "REDACTED_EMAIL", text)
-    text = re.sub(r"\b\d{10}\b", "REDACTED_PHONE", text)
-    return text
